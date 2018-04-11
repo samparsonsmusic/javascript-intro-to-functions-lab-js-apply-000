d94bd58489f561b36363402ae0e6aef73a6c3836
Sam Parsons <samparsonsmusic@gmail.com>
2018-04-11 00:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    return 
+    return "I can\'t hear you."
+  } else if (string === string.toUpperCase()) {
+    return ""
   }
 }
